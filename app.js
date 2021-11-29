e68a14aa561ffbbcb6cfa27dcd423267fb5dbf62
Yeonju Park <yeonjupark95@gmail.com>
2021-11-28 18:48:31 -0800

    enabled the feature to speed up/down while users are playing the game

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-//HTML ELements//
+// HTML Elements / Element structures
 const appElement = document.getElementById("app");
 const boardElement = document.createElement("div");
 const boardWrapperElement = document.createElement("div");
 const scoreParentElement = document.createElement("div");
 const showScoreElement = document.createElement("p");
-const titleDivElement = document.createElement("div");
+const titleElement = document.createElement("div");
 const speedParentElement = document.createElement("div");
 const showSpeedElement = document.createElement("p");
 const gameStartElement = document.createElement("div");
@@ -12,15 +12,14 @@ const gameStartElementTitle = document.createElement("p");
 const gameStartElementMessage = document.createElement("p");
 const gameEndElement = document.createElement("div");
 const gameEndParagraphMessage = document.createElement("p");
+const arrowKeyElement = document.createElement("div");
+const arrowKeyParagraphMessage = document.createElement("p");
 const playButtonElement = document.createElement("button");
 const speedUpButton = document.createElement("button");
 const speedDownButton = document.createElement("button");
-const arrowKeyElement = document.createElement("div");
-const arrowKeyParagraphMessage = document.createElement("p");
 
-// state / mode/
-// const PBR(object=reference) vs PBV
-// default values
+// State / Mode
+// Default gameState values
 const gameState = {
   board: [],
   currentScore: 0,
@@ -28,7 +27,7 @@ const gameState = {
   gameInterval: null,
   currentSpeed: 150,
   food: [3, 12],
-  poision: [],
+  superFood: [],
   snake: {
     body: [
       //[y,x]
@@ -42,12 +41,12 @@ const gameState = {
   keyPressed: false,
 };
 
-//establishing the state of my board
+// Building the initial state of my board
 function buildInitialState() {
   gameState.board = [];
   for (let i = 0; i < 17; i++) {
     const row = [];
-    //j is column (y-axis)//
+    //j is column
     for (let j = 0; j < 17; j++) {
       row.push("");
     }
@@ -60,9 +59,8 @@ function buildInitialState() {
 function buildSnake() {
   for (let i = 0; i < gameState.snake.body.length; i++) {
     const currentSnakeBody = gameState.snake.body[i];
-    console.log("currentSnakeLocation:" + currentSnakeBody);
-    const currentSnakeY = currentSnakeBody[0]; //Y-axis: row index
-    const currentSnakeX = currentSnakeBody[1]; //X-axis: column index
+    const currentSnakeY = currentSnakeBody[0]; //Y: row index
+    const currentSnakeX = currentSnakeBody[1]; //X: column index
     gameState.board[currentSnakeY][currentSnakeX] = "snake";
   }
   const currentSnakeHeadY = gameState.snake.body[0][0];
@@ -73,17 +71,16 @@ function buildSnake() {
 function buildFood() {
   const currentFoodLocation = gameState.food;
   gameState.board[currentFoodLocation[0]][currentFoodLocation[1]] = "food";
-  console.log("currentFoodLocation:" + currentFoodLocation);
 }
 
-// bootstrapping //
+// Bootstrapping
 function bootStrap() {
-  //HTML classes
+  // HTML classes
   boardElement.classList.add("board");
   boardWrapperElement.classList.add("board-wrapper");
   scoreParentElement.classList.add("score");
   speedParentElement.classList.add("speed");
-  titleDivElement.classList.add("title");
+  titleElement.classList.add("title");
   playButtonElement.classList.add("play-button");
   speedUpButton.classList.add("speed-up-button");
   speedDownButton.classList.add("speed-down-button");
@@ -93,24 +90,30 @@ function bootStrap() {
   gameEndElement.classList.add("game-end-popup-modal");
   gameEndParagraphMessage.classList.add("game-end-popup-message");
 
-  appElement.appendChild(titleDivElement);
-  appElement.appendChild(scoreParentElement);
+  // HTML appendChild / app structure
+  appElement.appendChild(titleElement);
   appElement.appendChild(speedParentElement);
-  appElement.appendChild(boardWrapperElement); 
-  boardWrapperElement.appendChild(boardElement);
-  scoreParentElement.appendChild(showScoreElement);
-  speedParentElement.appendChild(showSpeedElement);
+  appElement.appendChild(arrowKeyElement);
+  appElement.appendChild(arrowKeyParagraphMessage);
   appElement.appendChild(playButtonElement);
   appElement.appendChild(speedUpButton);
   appElement.appendChild(speedDownButton);
+  // HTML appendChild / board structure
+  appElement.appendChild(boardWrapperElement);
+  boardWrapperElement.appendChild(boardElement);
+  // HTML appendChild / score structure
+  appElement.appendChild(scoreParentElement);
+  scoreParentElement.appendChild(showScoreElement);
+  speedParentElement.appendChild(showSpeedElement);
+  // HTML appendChild / game start popup structure
   appElement.appendChild(gameStartElement);
-  appElement.appendChild(gameEndElement);
   gameStartElement.appendChild(gameStartElementTitle);
   gameStartElement.appendChild(gameStartElementMessage);
+  // HTML appendChild / game end popup structure
+  appElement.appendChild(gameEndElement);
   gameEndElement.appendChild(gameEndParagraphMessage);
-  appElement.appendChild(arrowKeyElement);
-  appElement.appendChild(arrowKeyParagraphMessage);
 
+  // HTML innerText
   playButtonElement.innerText = "\nPlay";
   speedUpButton.innerText = `🔺🐇\nSpeed Up`;
   speedDownButton.innerText = `🔻🐢\nSpeed Down`;
@@ -119,13 +122,13 @@ function bootStrap() {
   gameStartElementTitle.innerText = `Let's play snake!`;
   gameStartElementMessage.innerText = `Press Play button to start\n Eat as many 🍎s you can to score\n Press 🔺🐇 Speed Up to go faster\n Press 🔻🐢 Speed Down to go slower`;
 
-  // functions
+  // Bootstrapping functions
   buildInitialState();
   renderBoard();
   moveSnake();
 }
 
-// render / views
+// Render / Views
 function renderBoard() {
   boardElement.innerHTML = "";
   for (let i = 0; i < gameState.board.length; i++) {
@@ -145,7 +148,7 @@ function renderBoard() {
         snakeHeadEmoji.classList.add("snake-head-emoji");
         snakeHeadEmoji.innerText = "👀";
         tile.appendChild(snakeHeadEmoji);
-      }
+      } 
       tile.classList.add("tile");
       boardElement.appendChild(tile);
     }
@@ -153,23 +156,24 @@ function renderBoard() {
 }
 
 function moveSnake() {
+  // Decide the next direction of the snake based on the Keypress event listener
   const snakeHead = [
     gameState.snake.body[0][0] + gameState.snake.nextDirection[0][0],
     gameState.snake.body[0][1] + gameState.snake.nextDirection[0][1],
   ];
   if (isGameEnded(snakeHead)) {
-    console.log("game Ended!!!");
+    // HTML Elements / showing the play button for an option to replay
     playButtonElement.classList.add("show-play-button");
     playButtonElement.classList.remove("hide-play-button");
-
+    // HTML Element / showing the Game End Pop Up with Score
     gameEndParagraphMessage.innerText = `Game Over! \n\n🍎: ${gameState.currentScore} \n 🏆: ${gameState.highestScore}`;
     gameEndElement.classList.add("show-game-end-popup");
     return;
   }
-  gameState.snake.body.unshift(snakeHead); //adding the head to the snake body
-  gameState.snake.body.pop(); //removing the tail
+  gameState.snake.body.unshift(snakeHead);
+  gameState.snake.body.pop();
+  // Adding the food to move while snake is moving
   const foodPosition = gameState.food;
-  //if the snake hits the food
   if (snakeHead[0] === foodPosition[0] && snakeHead[1] === foodPosition[1]) {
     moveFood();
     gameState.currentScore += 1;
@@ -178,8 +182,9 @@ function moveSnake() {
     }
     const snakeBodyLength = gameState.snake.body.length;
     const snakeTail = gameState.snake.body[snakeBodyLength - 1];
-    //duplicating and adding the tail to the snake
     gameState.snake.body.push(snakeTail);
+  }
+  if (gameState.currentScore>3){
   }
   gameState.keyPressed = false;
 }
@@ -188,34 +193,32 @@ function showScore() {
   showScoreElement.innerText = `🍎 ${gameState.currentScore} 🏆 ${gameState.highestScore}`;
 }
 
-//gets called in function moveSnake()
 function moveFood() {
-  //generate a random food location that's different from previous location or that's not part of the snake's body
+  // generate a random food location
   let randomFoodY = 0;
   let randomFoodX = 0;
   do {
     randomFoodY = Math.floor(Math.random() * gameState.board.length);
     randomFoodX = Math.floor(Math.random() * gameState.board.length);
-  } while (
+  } while ( // food location that's different from previous location or that's not part of the snake's body //
     (gameState.food[0] === randomFoodY && gameState.food[1] === randomFoodX) ||
     gameState.board[randomFoodY][randomFoodX] === "snake"
   );
   gameState.food = [randomFoodY, randomFoodX];
 }
 
-//gets called in function moveSnake()
 function isGameEnded(snakeHead) {
-  //check if snake goes outside vertically
+  // Check if snake goes outside the board vertically
   if (snakeHead[0] < 0 || snakeHead[0] > gameState.board.length - 1) {
     clearInterval(gameState.gameInterval);
     return true;
   }
-  //check if snake goes outside horizontally
+  // Check if snake goes outside the board horizontally
   if (snakeHead[1] < 0 || snakeHead[1] > gameState.board.length - 1) {
     clearInterval(gameState.gameInterval);
     return true;
   }
-  //check if the snake touches its body
+  // Check if the snake touches its own body
   for (let i = 0; i < gameState.snake.body.length; i++) {
     if (
       gameState.snake.body[i][0] === snakeHead[0] &&
@@ -227,45 +230,42 @@ function isGameEnded(snakeHead) {
   }
 }
 
-// listeners
+// Listeners / keydown
 document.addEventListener("keydown", function (event) {
   event.preventDefault();
+  // Remove the arrow key messages to user once users press the keys
   arrowKeyElement.classList.remove("arrow-key");
   arrowKeyParagraphMessage.classList.remove("arrow-key-message");
   arrowKeyParagraphMessage.innerText = "";
-  //if the game hasn't started yet, start the interval
+  // If the game hasn't started yet, start the interval when users press the keys
   if (gameState.gameInterval === null) {
     gameState.gameInterval = setInterval(tick, gameState.currentSpeed);
   }
   const nextDirectionY = gameState.snake.nextDirection[0][0];
   const nextDirectionX = gameState.snake.nextDirection[0][1];
-  // snake can't move towards the opposite directions //
+  // Changing next directions based on the keys, and snake can't move towards the opposite directions
   if (!gameState.keyPressed) {
     switch (event.key) {
       case "ArrowUp":
         if (nextDirectionY === 1 && nextDirectionX === 0) {
-          console.log("wrong direction");
           return;
         }
         gameState.snake.nextDirection = [[-1, 0]];
         break;
       case "ArrowDown":
         if (nextDirectionY === -1 && nextDirectionX === 0) {
-          console.log("wrong direction");
           return;
         }
         gameState.snake.nextDirection = [[1, 0]];
         break;
       case "ArrowLeft":
         if (nextDirectionY === 0 && nextDirectionX === 1) {
-          console.log("wrong direction");
           return;
         }
         gameState.snake.nextDirection = [[0, -1]];
         break;
       case "ArrowRight":
         if (nextDirectionY === 0 && nextDirectionX === -1) {
-          console.log("wrong direction");
           return;
         }
         gameState.snake.nextDirection = [[0, 1]];
@@ -275,14 +275,20 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
+// Listener / Play button
 playButtonElement.addEventListener("click", function () {
+  // HTML Element / Remove the arrow key messages to user once users press the play button
   arrowKeyElement.classList.add("arrow-key");
   arrowKeyParagraphMessage.classList.add("arrow-key-message");
   arrowKeyParagraphMessage.innerText = "Use your arrow keys to start moving";
+  // HTML Element / Remove the play button to user once users press the play button
   playButtonElement.classList.remove("show-play-button");
   playButtonElement.classList.add("hide-play-button");
+  // HTML Element / Remove the game start pop up
   gameStartElement.classList.add("hide-game-start-popup");
   gameEndElement.classList.remove("show-game-end-popup");
+
+  // Reset the snake & the current score
   gameState.currentScore = 0;
   gameState.snake.body = [
     //[y,x]
@@ -294,60 +300,11 @@ playButtonElement.addEventListener("click", function () {
   gameState.food = [3, 12];
   buildInitialState();
   renderBoard();
-  // setTimeout(function(){
-  // gameState.currentSpeed = 150;
-  // gameState.gameInterval = setInterval(tick, gameState.currentSpeed);
   gameState.snake.nextDirection = [[0, 1]];
-  // },3000)
   gameState.gameInterval = null;
-  console.log("currentSpeed:" + gameState.currentSpeed);
 });
 
-speedUpButton.addEventListener("click", function () {
-  // max speed at 90, min speed at 240
-  if (gameState.currentSpeed > 90 && gameState.currentSpeed <= 240) {
-    gameState.currentSpeed -= 30;
-  }
-  if (gameState.currentSpeed === 240) {
-    showSpeedElement.innerText = `🐢🐢🐢`;
-  } else if (gameState.currentSpeed === 210) {
-    showSpeedElement.innerText = `🐢🐢`;
-  } else if (gameState.currentSpeed === 180) {
-    showSpeedElement.innerText = `🐢`;
-  } else if (gameState.currentSpeed === 150) {
-    showSpeedElement.innerText = `🐇`;
-  } else if (gameState.currentSpeed === 120) {
-    showSpeedElement.innerText = `🐇🐇`;
-  } else if (gameState.currentSpeed === 90) {
-    showSpeedElement.innerText = `🐇🐇🐇`;
-  }
-  console.log("currentSpeedUpButton:" + gameState.currentSpeed);
-  // gameState.gameInterval = setInterval(tick, gameState.currentSpeed);
-});
-
-speedDownButton.addEventListener("click", function () {
-  // max speed at 90, min speed at 240
-  if (gameState.currentSpeed >= 90 && gameState.currentSpeed < 240) {
-    gameState.currentSpeed += 30;
-  }
-  if (gameState.currentSpeed === 240) {
-    showSpeedElement.innerText = `🐢🐢🐢`;
-  } else if (gameState.currentSpeed === 210) {
-    showSpeedElement.innerText = `🐢🐢`;
-  } else if (gameState.currentSpeed === 180) {
-    showSpeedElement.innerText = `🐢`;
-  } else if (gameState.currentSpeed === 150) {
-    showSpeedElement.innerText = `🐇`;
-  } else if (gameState.currentSpeed === 120) {
-    showSpeedElement.innerText = `🐇🐇`;
-  } else if (gameState.currentSpeed === 90) {
-    showSpeedElement.innerText = `🐇🐇🐇`;
-  }
-  console.log("currentSpeedDownButton:" + gameState.currentSpeed);
-  // gameState.gameInterval = setInterval(tick, gameState.currentSpeed);
-});
-
-// add to above
+// Incremental change that happens to the state every time
 function tick() {
   moveSnake();
   buildInitialState();
@@ -355,5 +312,59 @@ function tick() {
   showScore();
 }
 
-// global function call //
+// Listener / speed up button
+speedUpButton.addEventListener("click", function () {
+  // Max speed at 90, min speed at 240
+  if (gameState.currentSpeed > 90 && gameState.currentSpeed <= 240) {
+    gameState.currentSpeed -= 30;
+  }
+  // HTML Elements / updating the current speed for users to see
+  if (gameState.currentSpeed === 240) {
+    showSpeedElement.innerText = `🐢🐢🐢`;
+  } else if (gameState.currentSpeed === 210) {
+    showSpeedElement.innerText = `🐢🐢`;
+  } else if (gameState.currentSpeed === 180) {
+    showSpeedElement.innerText = `🐢`;
+  } else if (gameState.currentSpeed === 150) {
+    showSpeedElement.innerText = `🐇`;
+  } else if (gameState.currentSpeed === 120) {
+    showSpeedElement.innerText = `🐇🐇`;
+  } else if (gameState.currentSpeed === 90) {
+    showSpeedElement.innerText = `🐇🐇🐇`;
+  }
+  // If the game has already started, then update the interval
+  if (gameState.gameInterval !== null) {
+    clearInterval(gameState.gameInterval);
+    gameState.gameInterval = setInterval(tick, gameState.currentSpeed);
+  }
+});
+
+// Listener / speed down button
+speedDownButton.addEventListener("click", function () {
+  // Max speed at 90, min speed at 240
+  if (gameState.currentSpeed >= 90 && gameState.currentSpeed < 240) {
+    gameState.currentSpeed += 30;
+  }
+  // HTML Elements / updating the current speed for users to see
+  if (gameState.currentSpeed === 240) {
+    showSpeedElement.innerText = `🐢🐢🐢`;
+  } else if (gameState.currentSpeed === 210) {
+    showSpeedElement.innerText = `🐢🐢`;
+  } else if (gameState.currentSpeed === 180) {
+    showSpeedElement.innerText = `🐢`;
+  } else if (gameState.currentSpeed === 150) {
+    showSpeedElement.innerText = `🐇`;
+  } else if (gameState.currentSpeed === 120) {
+    showSpeedElement.innerText = `🐇🐇`;
+  } else if (gameState.currentSpeed === 90) {
+    showSpeedElement.innerText = `🐇🐇🐇`;
+  }
+  // If the game has already started, then update the interval
+  if (gameState.gameInterval !== null) {
+    clearInterval(gameState.gameInterval);
+    gameState.gameInterval = setInterval(tick, gameState.currentSpeed);
+  }
+});
+
+// Global Function Call /
 bootStrap();
